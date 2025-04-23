cb48892274b805ab3fbcfec8d834240d861f1771
Decency Okobia <learncodingdee@yahoo.com>
2025-04-23 19:42:17 +0100

    sort order done

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import SortingSelector from "./Components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -46,7 +47,12 @@ function App() {
               setGameQuery({ ...gameQuery, platform })
             }
           />
-          <SortingSelector />
+          <SortingSelector
+            sortOrder={gameQuery.sortOrder}
+            onSelectSortOrder={(sortOrder) =>
+              setGameQuery({ ...gameQuery, sortOrder })
+            }
+          />
         </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
