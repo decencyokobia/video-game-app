3649f9288a4a1dbf62a76375e709561b69051683
Decency Okobia <learncodingdee@yahoo.com>
2025-04-23 20:27:23 +0100

    implement searching

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,11 +3,15 @@ import cherry from "../assets/cherry.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const Navbar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const Navbar = ({ onSearch }: Props) => {
   return (
     <HStack padding={"10px"}>
       <Image src={cherry} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
